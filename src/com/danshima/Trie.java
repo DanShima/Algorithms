46cd01db004e2d4542eac98170aad96804b30d70
DanShima <giddy_with_panda@hotmail.com>
2017-12-31 23:09:47 +0100

    finished Trie insert and search methods

--- src/com/danshima/Trie.java
+++ src/com/danshima/Trie.java
@@ -9,18 +9,102 @@ package com.danshima;
  */
 
 public class Trie {
-    private TrieNode root;
+    static TrieNode root;
 
     public Trie(){
         root = new TrieNode();
     }
-    
-    class TrieNode {
-        TrieNode[] arr;
-        boolean isEnd;
+
+    static class TrieNode {
+        //Each trie node can only contains 'a'-'z' characters thus use a small array to store the character.
+        TrieNode[] children;
+        boolean isEndOfWord;
         public TrieNode() {
-            this.arr = new TrieNode[26];
+            this.children = new TrieNode[26];
+        } //alphabet size is 26
+
+    }
+
+    //inserts a word into the trie
+    public static void insert(String word) {
+        TrieNode p = root;
+        for(int i = 0; i < word.length(); i++){
+            int index = word.charAt(i) - 'a';
+            if(p.children[index] == null) {
+                TrieNode temp = new TrieNode();
+                p.children[index] = temp;
+                p = temp;
+            }else{
+                p = p.children[index];
+            }
         }
+        p.isEndOfWord = true;
+    }
+
+
+
+    //returns if a the word is in the trie
+    public static boolean search(String word){
+        TrieNode enteredWord = searchNode(word);
+        if(enteredWord == null){
+            return false;
+        } else {
+            if(enteredWord.isEndOfWord){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static TrieNode searchNode(String searchWord){
+        TrieNode wordArray = root;
+        for(int i = 0; i < searchWord.length(); i++){
+            int index = searchWord.charAt(i) - 'a';
+            if(wordArray.children[index] != null){
+                wordArray = wordArray.children[index];
+            } else {
+                return null;
+            }
+        }
+        if (wordArray == root){
+            return null;
+
+        }
+        return wordArray;
+    }
+
+
+    public static void main(String args[])
+    {
+        // Input keys (use only 'a' through 'z' and lower case)
+        String keys[] = {"thaw", "a", "there", "answer", "any",
+                "by", "bye", "their"};
+
+        String output[] = {"Not present in trie", "Present in trie"};
+
+
+        root = new TrieNode();
+
+        // Construct trie
+        int i;
+        for (i = 0; i < keys.length ; i++)
+            insert(keys[i]);
+
+        // Search for different keys
+        if(search("the") == true)
+            System.out.println("the --- " + output[1]);
+        else System.out.println("the --- " + output[0]);
+
+        if(search("these") == true)
+            System.out.println("these --- " + output[1]);
+        else System.out.println("these --- " + output[0]);
+
+        if(search("their") == true)
+            System.out.println("their --- " + output[1]);
+        else System.out.println("their --- " + output[0]);
+
+        if(search("thaw") == true)
+            System.out.println("thaw --- " + output[1]);
+        else System.out.println("thaw --- " + output[0]);
 
     }
 }
