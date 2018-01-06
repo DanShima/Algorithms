6c3b3c9786dd50fe191a1d1ec8ff6b3e47a39a0c
DanShima <giddy_with_panda@hotmail.com>
2018-01-06 23:23:57 +0100

    Fibonacci methods

--- src/com/danshima/SingleExercises.java
+++ src/com/danshima/SingleExercises.java
@@ -24,4 +24,36 @@ public class SingleExercises {
             return n * factorial(n -1);
         }
     }
+
+    //computes the Nth Fibonacci number. it takes O(2 to the power of n)
+    int fib(int n){
+        if(n <= 0) return 0;
+        else if(n ==1) return 1;
+        return fib(n -1) + fib(n - 2);
+    }
+
+    //print all Fibonacci numbers from 0 to n.
+    void allFib(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println(i + ": " + fib(i));
+        }
+    }
+
+    //print the powers of 2 from 1 through n (inclusive)
+    //divide n by 2 untill we get down to the base case (1). the runtime is O(log n).
+    int powersOf2(int n){
+        if(n < 1) {
+            return 0;
+        }else if(n == 1){
+            System.out.println(1);
+            return 1;
+        } else {
+            int previous = powersOf2(n / 2);
+            int current = previous * 2;
+            System.out.println(current);
+            return current;
+        }
+
+    }
+
 }
